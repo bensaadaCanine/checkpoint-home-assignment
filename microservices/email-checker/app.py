306cb33184c42d025a98e34b82cdc15a7c50e7fb
Ben Saada <bensaada12@gmail.com>
2026-03-11 11:49:32 +0200

    ⚽️ work in progress Wed Mar 11 11:49:32 2026

--- microservices/email-checker/app.py
+++ microservices/email-checker/app.py
@@ -2,15 +2,27 @@ import json
 import os
 
 import boto3
+import logger
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s [%(levelname)s] [%(name)s] %(message)s",
+    handlers=[logging.StreamHandler(sys.stdout)],
+)
+
+logger = logging.getLogger(__name__)
+
 app = FastAPI()
 
+logger.info("Initializing boto3 clients")
 sqs = boto3.client("sqs", region_name=os.getenv("AWS_REGION", "eu-west-1"))
 ssm = boto3.client("ssm", region_name=os.getenv("AWS_REGION", "eu-west-1"))
 
 QUEUE_URL = os.environ.get("QUEUE_URL")
+
+logger.info("Retrieving validation token from SSM")
 TOKEN_PARAM = ssm.get_parameter(
     Name="/email-checker/validation-token", WithDecryption=True
 )["Parameter"]["Value"]
@@ -31,9 +43,22 @@ class RequestModel(BaseModel):
 @app.post("/publish")
 def publish(request: RequestModel):
 
+    logger.info("Dealing with incoming message")
     if request.token != TOKEN_PARAM:
+        logger.error("Message has invalid token")
         raise HTTPException(status_code=401, detail="Invalid token")
 
+    logger.info("Sending message to queue")
     sqs.send_message(QueueUrl=QUEUE_URL, MessageBody=request.json())
 
     return {"status": "Message sent to queue"}
+
+
+@app.get("/health")
+def health():
+    return {"status": "ok"}
+
+
+@app.get("/ready")
+def readiness():
+    return {"status": "ready"}
